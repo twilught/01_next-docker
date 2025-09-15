01d59708d7ab31a3ca96c8f9f55281834c921011
twilught <kongba5555@gmail.com>
2025-09-15 14:29:02 +0700

    finr

--- app/components/Attractions.js
+++ app/components/Attractions.js
@@ -48,7 +48,7 @@ export default function Attractions() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold text-center mb-10 text-gray-800">
-        Popular Tourist Attractions
+        Athip buasamlee 6605086
       </h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
